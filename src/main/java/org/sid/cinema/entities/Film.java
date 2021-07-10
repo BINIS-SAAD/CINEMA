9a20c2b3c59cebdcc3fd8ca08a9a7cde2fdef118
Binis Saad <bin.saad@hotmail.fr>
2021-07-10 19:22:40 +0100

    First Commit Uploading All Files

---
+++ src/main/java/org/sid/cinema/entities/Film.java
@@ -0,0 +1,31 @@
+package org.sid.cinema.entities;
+
+import java.util.Collection;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor
+public class Film {
+@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String titre;
+	private String description;
+	private String realisateur;
+	private Date dateSortie;
+	private double duree;
+	private String photo;
+	@ManyToOne
+	private Categorie categorie;
+	@OneToMany(mappedBy = "film")
+	private Collection<Projection> projections;
+}
